f78e2bbde13876fa5b3f79440e03e486d80464d6
Oliver Joseph Ash <oliverjash@icloud.com>
2014-05-22 12:40:51 +0100

    Add gulp watch and livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
+var livereload = require('gulp-livereload');
 
-gulp.task('default', function () {
-    gulp.src('./src/css/*.scss')
+gulp.task('sass', function () {
+    gulp.src('./src/css/main.scss')
         .pipe(sass())
         .pipe(gulp.dest('./target/css'));
 });
+
+gulp.task('watch', function () {
+    var server = livereload();
+    gulp.watch('./src/css/**/*.scss', ['sass']);
+    gulp.watch('./target/css/**').on('change', function (file) {
+        server.changed(file.path);
+    });
+});
+
+gulp.task('default', ['sass', 'watch']);
