161cd1fadde5de6cc11c37c637a56a6b5ac7a149
Oliver Joseph Ash <oliverjash@icloud.com>
2014-05-23 17:18:19 +0100

    Tidy up

--- build.js
+++ build.js
@@ -8,7 +8,33 @@ var ejs = require('ejs');
 var fs = require('fs');
 var path = require('path');
 
-var filenames = ['./src/views/index.ejs', './src/views/open-source.ejs'];
+var basePath = './src/views';
+var filenames = ['index.ejs', 'open-source.ejs'].map(function (f) {
+    return [basePath, f].join('/');
+});
+
+var pages = [
+    {
+        title: 'Home',
+        filename: 'index.html'
+    },
+    {
+        title: 'Open Platform',
+        filename: 'open-platform.html'
+    },
+    {
+        title: 'Open Source',
+        filename: 'open-source.html'
+    },
+    {
+        title: 'Events & Talks',
+        filename: 'events-&-talks.html'
+    },
+    {
+        title: 'Join the Team',
+        filename: 'join-the-team.html'
+    }
+];
 
 filenames.forEach(function (filename) {
     var file = fs.readFileSync(filename, { encoding: 'utf8' });
@@ -16,30 +42,10 @@ filenames.forEach(function (filename) {
     var output = ejs.render(file, {
         filename: filename,
         fileBasename: fileBasename,
-        pages: [
-            {
-                title: 'Home',
-                filename: 'index.html'
-            },
-            {
-                title: 'Open Platform',
-                filename: 'open-platform.html'
-            },
-            {
-                title: 'Open Source',
-                filename: 'open-source.html'
-            },
-            {
-                title: 'Events & Talks',
-                filename: 'events-&-talks.html'
-            },
-            {
-                title: 'Join the Team',
-                filename: 'join-the-team.html'
-            }
-        ]
+        pages: pages
     });
 
-    fs.writeFileSync('./target/' + fileBasename.replace(/\.ejs$/, '.html'), output, { encoding: 'utf8' });
+    fs.writeFileSync('./target/' + fileBasename.replace(/\.ejs$/, '.html'),
+        output, { encoding: 'utf8' });
 });
 
