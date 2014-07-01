4a03318317c1efcecb6f38d9eaf6dfb1f4a037a5
Oliver Joseph Ash <oliverjash@icloud.com>
2014-07-01 17:20:27 +0100

    Bump jspm packages

--- src/config.js
+++ src/config.js
@@ -23,13 +23,13 @@ System.config({
     "npm:base64-js@0.0.4": {},
     "npm:Base64@0.2.1": {},
     "github:systemjs/plugin-text": "github:systemjs/plugin-text@master",
-    "moment": "github:moment/moment@^2.6.0"
+    "moment": "github:moment/moment@^2.7.0"
   }
 });
 
 System.config({
   "versions": {
-    "github:ded/reqwest": "1.1.0",
+    "github:ded/reqwest": "1.1.1",
     "npm:ejs": "1.0.0",
     "github:jspm/nodelibs": "0.0.2",
     "npm:inherits": "2.0.1",
@@ -38,7 +38,7 @@ System.config({
     "npm:Base64": "0.2.1",
     "github:systemjs/plugin-json": "master",
     "github:systemjs/plugin-text": "master",
-    "github:moment/moment": "2.6.0"
+    "github:moment/moment": "2.7.0"
   }
 });
 
