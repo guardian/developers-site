a3cbcda8ac1940ed8d30b10fd2f7e75fa48fcf2d
Oliver Joseph Ash <oliverjash@icloud.com>
2014-07-07 16:01:50 +0100

    Test for npm and jspm

--- setup.sh
+++ setup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-set -o errexit
+test $(which npm)
+if [ $? != "0" ]; then
+    echo -e "npm not found: please install Node.js"
+    exit 1
+fi
+
+test $(which jspm2)
+if [ $? != "0" ]; then
+    echo -e "jspm not found: please run 'sudo npm install -g jspm'"
+    exit 1
+fi
 
 BASE="`dirname \"$0\"`"
+
+set -o errexit
 
 bundle install --gemfile $BASE/Gemfile-sass
 
