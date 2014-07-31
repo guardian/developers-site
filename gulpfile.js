8826f002355aa15e8c6e3a4c4c2e3bc89696ea03
Oliver Joseph Ash <oliverjash@icloud.com>
2014-07-31 11:00:37 +0100

    Correct capitalisation

--- gulpfile.js
+++ gulpfile.js
@@ -87,8 +87,8 @@ var pages = [
         talks: talks
     },
     {
-        title: "Join the Guardian’s development team",
-        menuTitle: "Join the team",
+        title: "Join the Guardian’s Development Team",
+        menuTitle: "Join the Team",
         fileBasename: 'join-the-team.ejs',
         description: "The Guardian is the world’s leading liberal voice, come join the team of developers behind the Pulitzer Prize-winning site theguardian.com",
         jobs: jobs
